31fd8d373157801e1cab1509906cb0af59e27014
Hannah Catherine Trask <hctrask@gmail.com>
2021-09-02 10:42:07 -0600

    deploy

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head"
 import stylesheet from 'styles/main.scss'
-
 import Header from "../components/Header"
 import Main from "../components/Main"
 import Footer from "../components/Footer"
