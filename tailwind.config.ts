f482a7bb0d746c190ad659618defff0c0e913252
Hannah Catherine Trask <hannah@trynow.io>
2024-02-14 10:50:18 -0700

    First commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,16 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+        "light",
+        "dark",
+        "retro",
+        "aqua",
+        "cupcake"
+    ]
+  }
 };
+
 export default config;
