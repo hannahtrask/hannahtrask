27642efee7d6214dad026cb0d4b70d12bbb696d5
Hannah Catherine Trask <hctrask@gmail.com>
2024-04-19 18:13:00 -0600

    Adds instagram, fixes animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,21 @@ import Socials from "@/components/Socials";
 export default function Home() {
   return (
     <main>
-      <div className="intro-title">
-        <div className="slide-in-left">
-          <h1>Hey, I&apos;m Hannah!</h1>
-          <p>I&apos;m a software engineer.</p>
-          <h6>(among other things)</h6>
-        </div>
-        <div className="slide-in-right">
-          <Image
-            src={avatar}
-            alt="absolutely ripping a backside turn"
-            width={300}
-            height={300}
-          />
+      <div className="main-body">
+        <div className="intro-title">
+          <div className="slide-in-left">
+            <h1>Hey, I&apos;m Hannah!</h1>
+            <p>I&apos;m a software engineer.</p>
+            <h6>(among other things)</h6>
+          </div>
+          <div className="slide-in-right">
+            <Image
+              src={avatar}
+              alt="absolutely ripping a backside turn"
+              width={300}
+              height={300}
+            />
+          </div>
         </div>
       </div>
       <Socials />
--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,4 +1,5 @@
 import {
+  InstagramIcon,
   LinkedinIcon,
   LinkedinShareButton,
   PinterestIcon,
@@ -7,19 +8,26 @@ import {
   WhatsappShareButton,
 } from "next-share";
 import React from "react";
+import Link from "next/link";
 
 export default function Socials() {
   return (
-    <div className="slide-in-left">
+    <div className="socials">
       <LinkedinShareButton url={"https://www.linkedin.com/in/hannahtrask/"}>
-        <LinkedinIcon size={32} round />
+        <LinkedinIcon size={32} round bgStyle={{ fill: "grey" }} />
       </LinkedinShareButton>
       <WhatsappShareButton url={"https://wa.link/cmibd5"}>
-        <WhatsappIcon size={32} round />
+        <WhatsappIcon size={32} round bgStyle={{ fill: "grey" }} />
       </WhatsappShareButton>
-      <PinterestShareButton url={"https://www.pinterest.com/hellaahannah/"} media={'some style vibes'}>
-        <PinterestIcon size={32} round />
+      <PinterestShareButton
+        url={"https://www.pinterest.com/hellaahannah/"}
+        media={"some style vibes"}
+      >
+        <PinterestIcon size={32} round bgStyle={{ fill: "grey" }} />
       </PinterestShareButton>
+      <Link href={"https://www.instagram.com/hannah_____catherine/"}>
+        <InstagramIcon size={32} round bgStyle={{ fill: "grey" }} />
+      </Link>
     </div>
   );
 }
