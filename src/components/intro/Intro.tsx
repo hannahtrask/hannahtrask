3bf5092570c5bc7e08767a42a3be599e789ea89f
Hannah Catherine Trask <hctrask@gmail.com>
2024-04-30 13:25:57 -0600

    Adds framer motion

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -1,18 +1,31 @@
+"use client";
 import Image from "next/image";
 import profile from "../../../public/assets/profile-foto.png";
 import classes from "./intro.module.css";
+import { motion } from "framer-motion";
 
 export function Intro() {
   return (
     <div className={classes.intro}>
-      <Image
-        src={profile}
-        alt="photo of me"
-        width={300}
-        height={300}
-        style={{ margin: `1rem` }}
-      />
-      <div>
+      <motion.div
+        initial={{ x: -300, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.75, delay: 0.5, ease: "easeOut" }}
+        viewport={{ once: true }}
+      >
+        <Image
+          src={profile}
+          alt="photo of me"
+          width={300}
+          height={300}
+          style={{ margin: `1rem` }}
+        />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.25, ease: "easeOut" }}
+      >
         <h1>Who am I?</h1>
         <p>
           Hey, I’m Hannah. I’m a software engineer, content writer, and blogger—
@@ -20,7 +33,7 @@ export function Intro() {
           software engineer.. you name it. I thrive in environments where my
           projects are ever-changing, and I LOVE learning new things.
         </p>
-      </div>
+      </motion.div>
     </div>
   );
 }
