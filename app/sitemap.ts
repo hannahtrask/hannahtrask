623dd9e667c010c60d22a0e7e7c7592566e48921
hannahtrask <hctrask@gmail.com>
2026-02-03 11:33:45 -0700

    Remove from sitemap projects pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,21 +1,10 @@
 import { MetadataRoute } from 'next'
-import { client } from '@/sanity/client'
-import { SanityDocument } from 'next-sanity'
 
-const PROJECTS_QUERY = `*[_type == "project" && defined(slug.current)] {
-  slug,
-  _updatedAt
-}`
-
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl =
     process.env.NEXT_PUBLIC_SITE_URL || 'https://wearesagebrush.com'
 
-  // Get all projects from Sanity
-  const projects = await client.fetch<SanityDocument[]>(PROJECTS_QUERY)
-
-  // Static pages
-  const staticPages: MetadataRoute.Sitemap = [
+  return [
     {
       url: baseUrl,
       lastModified: new Date(),
@@ -47,14 +36,4 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.3,
     },
   ]
-
-  // Dynamic project pages
-  const projectPages: MetadataRoute.Sitemap = projects.map(project => ({
-    url: `${baseUrl}/project/${project.slug.current}`,
-    lastModified: new Date(project._updatedAt),
-    changeFrequency: 'monthly' as const,
-    priority: 0.6,
-  }))
-
-  return [...staticPages, ...projectPages]
 }
